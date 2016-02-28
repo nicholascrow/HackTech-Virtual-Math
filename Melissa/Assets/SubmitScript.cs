309d579eb1ed3e8ab5715a0dde6a0f42a2513216
Your Name <melissa.c.apricotfrog@gmail.com>
2016-02-27 23:01:59 -0800

    Yo

---
+++ Melissa/Assets/SubmitScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onCollisionEnter(Collision col)
+	{
+		// If the candy has collided with the "submit" button
+		if (col.gameObject.tag.Equals ("NotInBox")) {
+			gameObject.tag = "submitted"; // Set the tag to InBox
+		}
+	}
+}
